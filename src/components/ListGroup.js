eaf8814a9fad47d76e9d3cd1c643beac9d264560
Yaman KATBY <m.yaman.katby@gmail.com>
2019-11-22 16:08:40 +0300

    💄 Work on the TodoList page UI

---
+++ src/components/ListGroup.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ListGroupItem = (props) => {
+	return (
+		<li className='list-group-item list-group-item-action d-flex justify-content-between align-items-center'>
+			{props.title}
+			<span className={`badge-pill badge-${props.tintColor}`}>
+          {props.count}
+        </span>
+		</li>
+	);
+};
+
+const ListGroup = () => {
+	return (
+		<ul className='list-group list-group-horizontal-md'>
+			<ListGroupItem title='All' count={5} tintColor='primary' />
+			<ListGroupItem title='Completed' count={3} tintColor='warning' />
+			<ListGroupItem title='Uncompleted' count={2} tintColor='danger' />
+		</ul>
+	);
+};
+
+export default ListGroup;
---
+++ src/components/Table.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Table = () => {
+	return (
+		<div className='card mt-3'>
+			<div className='card-header d-flex justify-content-between align-items-center'>
+				TodoList
+				<button className='btn btn-primary btn-sm'>Create</button>
+			</div>
+			<div className='card-body'>
+				<table className='table table-bordered table-hover'>
+					<thead>
+					<tr>
+						<th>Text</th>
+						<th>Completed</th>
+						<th style={{ width: 182 }}>Options</th>
+					</tr>
+					</thead>
+					<tbody>
+					<tr>
+						<td>Learn React</td>
+						<td>Not yet</td>
+						<td>
+							<div className='btn-group btn-group-sm'>
+								<button className='btn btn-primary'>Toggle</button>
+								<button className='btn btn-primary'>Edit</button>
+								<button className='btn btn-danger'>Remove</button>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>Learn HTML</td>
+						<td>Yep!</td>
+						<td>
+							<div className='btn-group btn-group-sm'>
+								<button className='btn btn-primary'>Toggle</button>
+								<button className='btn btn-primary'>Edit</button>
+								<button className='btn btn-danger'>Remove</button>
+							</div>
+						</td>
+					</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
+};
+
+export default Table;
